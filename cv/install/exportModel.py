43d33405ce8f3b8ec012555b5a59384af7bcfd8d
Josh M <eel.josh@gmail.com>
2024-06-12 09:01:43 -0700

    Got a model to 603kB

---
+++ cv/install/exportModel.py
@@ -0,0 +1,59 @@
+#
+# Sensor Fusion
+#
+# MIC Lab
+# Joshua B. Mehlman
+# Summer 2024
+#
+###
+#
+# Save the model to TensorFlow MicroControler
+#
+###
+from pathlib import Path
+from ultralytics import YOLO
+import torch
+
+
+def saveModel(model, imgLayers, imgWidth, imgHeight ):
+    name = "best"
+    # train13, v8  : 6.5MB
+    # trainv14, 5.6: 8.9MB
+    modelDir = "runs/detect/train21/weights/" 
+    modelPath = Path(modelDir)
+    fileName = name+".pt"
+    modelFile = modelPath/fileName
+
+    # Load the state dict
+    #model = YOLO("models/yolov8n.yaml" )  # build a new model from YAML
+    # v8: Opened as 12.1MB
+    # v5.6: 17.4
+    model.load_state_dict(torch.load(modelFile), strict=False) 
+
+    # Save as ONNX
+    #https://docs.ultralytics.com/modes/export/#arguments
+    #model.export(format="onnx", imgsz=imgHeight, int8=True, , optimize=True)
+    '''
+    onnxFileName = name+".onnx"
+    onnxFile = modelPath/onnxFileName
+    print(f"Saveing to Onnx: {onnxFile}")
+    input_shape = (1, imgLayers, imgWidth, imgHeight)
+    #exit()
+    torch.onnx.export(model, torch.randn(input_shape), onnxFile, verbose=True,
+                      input_names=["input"],
+                      output_names=["output"])
+    '''
+
+    # Convert to Tensorflow
+
+    # Convert to TF Lite
+    # barking: ModuleNotFoundError: No module named 'imp'
+    # Run with python < 3.12: https://docs.python.org/3.11/library/imp.html
+    # But it barks AFTER it saves the tfLite
+    model.export(format="tflite", imgsz=imgHeight, int8=True, data="coco8.yaml", optimize=True) # saved as 3.2MB
+
+model = YOLO("models/yolov8n.yaml" )  # build a new model from YAML
+image_depth = 3
+mean = 0
+std = 0
+saveModel(model, image_depth, 96, 96)
--- cv/training/train.py
+++ cv/training/train.py
@@ -12,7 +12,6 @@
 ###
 
 from ultralytics import YOLO
-from torchinfo import summary
 
 # Training data
 image_depth = 3
@@ -21,20 +20,15 @@ image_height = image_width
 
 # Load a model
 # https://github.com/ultralytics/ultralytics/blob/main/ultralytics/cfg/models/
-model = YOLO("models/yolov3-tiny.yaml")  # build a new model from YAML
-#model = YOLO("models/yolov8.yaml")  # build a new model from YAML
+#model = YOLO("models/yolov3-tiny.yaml")  # build a new model from YAML
+#model = YOLO("models/yolov5-p6n.yaml")  # build a new model from YAML
+#model = YOLO("models/yolov6n.yaml")  # build a new model from YAML
+model = YOLO("models/yolov8n.yaml")  # build a new model from YAML
+#model = YOLO("models/yolov8-p6n.yaml")  # build a new model from YAML
 #model = YOLO("models/yolov8n.pt")  # load a pretrained model (recommended for training)
 #model = YOLO("models/yolov8n.yaml").load("models/yolov8n.pt")  # build from YAML and transfer weights
 
-model.info()
+model.info(detailed=True)
 
-results = model.train(data="coco8.yaml", epochs=100, imgsz=640, device="mps")
+results = model.train(data="coco8.yaml", epochs=1, imgsz=image_width, device="mps")
 
-'''
-modelSum = summary(model=model, 
-            input_size=(1, image_depth, image_width, image_height), # make sure this is "input_size", not "input_shape"
-            col_names=["input_size", "output_size", "num_params", "trainable"],
-            col_width=20,
-            row_settings=["var_names"]
-            )
-'''
