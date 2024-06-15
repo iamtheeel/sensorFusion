bff7986f5d81d8e51074997b2771e3885cea8aec
Josh M <eel.josh@gmail.com>
2024-06-15 12:01:33 -0700

    Cleaned up getThisImgLabs

--- cv/training/viewer.py
+++ cv/training/viewer.py
@@ -6,24 +6,11 @@ import torchvision.transforms as trans
 
 from getLab import getLab
 
-def getThisLab(fileName):
-    print(f"getThisLab file: {fileName}")
 
-    image = cv2.imread(imageFile_str) 
-    nLab = labeler.getNLab(imageFile_str) # Sets up file structure for this file, and returns the number of lables
-    print(f"getThisLab num labels: {nLab}")
-    for thisLab in range(nLab):
-        label, UL, LR = labeler.getLabBox(thisLab)
-        print(f"label: {thisLab}, {label}")
-        cv2.rectangle(img=image, pt1=UL, pt2=LR, color=(0,255,0), thickness=1)
-        cv2.putText(img=image, text=label, org=UL, fontFace=cv2.FONT_HERSHEY_COMPLEX, fontScale=0.5, color=(255, 255, 255))
-
-    numpy_arr = np.asarray(image)
-    cv2.imshow('', numpy_arr)
-    cv2.waitKey()
-
+# Settings
 modelType = 'ssd' # is this yolo or ssd?
 #modelType = 'yolo' # is this yolo or ssd?
+
 #thisFile = None
 thisFile = 'dataset3_0.jpg'
 
@@ -39,18 +26,35 @@ else:
     image_dir = baseDir + '/' + dataSet + '/images/' + testTrain
 
 print(f"{image_dir}")
-labeler = getLab(modelType, baseDir, dataSet) # ssd, or yolo
+
+def getThisImgLabs(image_loc, fileName):
+    print(f"getThisLab file: {fileName}")
+
+    imageFile_str =image_loc+'/'+fileName
+    image = cv2.imread(imageFile_str) 
+    nLab = labeler.getNLab(imageFile_str) # Sets up file structure for this file, and returns the number of lables
+    print(f"getThisLab num labels: {nLab}")
+
+    # Add the labels and boxes to the image
+    for thisLab in range(nLab):
+        label, UL, LR = labeler.getLabBox(thisLab)
+        print(f"label: {thisLab}, {label}")
+        cv2.rectangle(img=image, pt1=UL, pt2=LR, color=(0,255,0), thickness=1)
+        cv2.putText(img=image, text=label, org=UL, fontFace=cv2.FONT_HERSHEY_COMPLEX, fontScale=0.5, color=(255, 255, 255))
+
+    # Display the image
+    numpy_arr = np.asarray(image)
+    cv2.imshow(fileName, numpy_arr)
+    cv2.waitKey()
+
+# Entry point
+labeler = getLab(modelType, baseDir, dataSet)  
 
 if thisFile != None:
-    imageFile_str =image_dir+'/'+thisFile
-    getThisLab(imageFile_str)
+    getThisImgLabs(image_dir, thisFile)
 
 else:
     listing = os.scandir(image_dir)
     for file in listing:
         if fnmatch.fnmatch(file, '*.jpg'):
-
-            # TODO make function so we can add single image variant
-            imageFile_str =image_dir+'/'+file.name
-            #print(f"{imageFile_str}")
-            getThisLab(imageFile_str)
+            getThisImgLabs(image_dir, file.name)
