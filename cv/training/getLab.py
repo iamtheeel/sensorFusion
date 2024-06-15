6e765401fb101664e75304ffee792d73a393350b
Josh M <eel.josh@gmail.com>
2024-06-15 11:52:19 -0700

    Viewer works with ssd labels. Viewer has single image mode

--- cv/training/getLab.py
+++ cv/training/getLab.py
@@ -3,6 +3,7 @@
 import os
 import yaml
 import cv2
+import xml.etree.ElementTree as ET
 
 class getLab:
     def __init__(self, type, baseDir, dataSet) -> None:
@@ -12,6 +13,10 @@ class getLab:
         self.labFile = ''
         self.imgFile = ''
 
+        #self.xmlTree = None 
+        self.xmlRoot = None
+
+        print(f"type: {type}")
         if type == 'ssd':
             # class name in label file
             self.ext = 'xml'
@@ -35,41 +40,84 @@ class getLab:
         fileExt = file_ext[0] + '.' + self.ext
 
         if self.type == 'ssd':
+            nLab = 0 
+
             labDir = head_tail[0]
+            print(f"labDir: {labDir}")
+            self.labFile = labDir + '/' + '/'+ fileExt
+            print(f"Label file: {self.labFile}")
+
+            xmlTree = ET.parse(self.labFile)
+            self.xmlRoot = xmlTree.getroot()
+            #for child in self.xmlRoot:
+            #     print(f"tag: {child.tag}, attrib: {child.attrib}")
+            #     if child.tag == 'object':
+            #        nLab += 1
+            for object in self.xmlRoot.findall('object'):
+                nLab += 1
+
         else:
             train_val = os.path.split(head_tail[0])
             lab_dir = os.path.split(train_val[0]) 
             labDir = lab_dir[0]+ '/labels'
+            self.labFile = labDir + '/' + train_val[1] + '/'+ fileExt
 
-        self.labFile = labDir + '/' + train_val[1] + '/'+ fileExt
-        #print(f"Label file: {self.labFile}")
-
-        with open(self.labFile, 'r') as fp: 
-            nLab = len(fp.readlines())
+            print(f"Label file: {self.labFile}")
+            with open(self.labFile, 'r') as fp: 
+                nLab = len(fp.readlines())
 
         return nLab
+
     
     def getLabBox(self, labNum):
-        with open(self.labFile, 'r') as fp: 
+        if self.type == 'ssd':
+            label, UL, LR = self.getLabBoxSSD(labNum)
+        else:
             img = cv2.imread(self.imgFile)
             imgH, imgW, imgC = img.shape
-            #print(f"h: {imgH}, w: {imgW}, ch: = {imgC}")
+            label, UL, LR = self.getLabBoxYOLO(labNum, imgH, imgW, imgC)
+
+        return label, UL, LR
+
+    def getLabBoxSSD(self, labNum):
+        thisLab = 0
+        #print(f"getLabBoxSSD labNum: {labNum}")
+        for object in self.xmlRoot.findall('object'):
+            if thisLab == labNum:
+                name = object.find('name').text
+                #print(f"getLabBoxSSD label: {name}")
+
+                bndBox = object.find('bndbox')
+                xmin = int(float(bndBox.find('xmin').text))
+                xmax = int(float(bndBox.find('xmax').text))
+                ymin = int(float(bndBox.find('ymin').text))
+                ymax = int(float(bndBox.find('ymax').text))
+                #print(f"xmin: {xmin}, xmax: {xmax}, ymin: {ymin}, ymax: {ymax}")
+                return name, (xmin,ymin), (xmax,ymax)
+            thisLab += 1
+
+    def getLabBoxYOLO(self, labNum, imgH, imgW, imgC):
+        # label, x, y, w, h
+        #print(f"h: {imgH}, w: {imgW}, ch: = {imgC}")
+        with open(self.labFile, 'r') as fp: 
             lines = fp.readlines()
-            #print(f"getLabBox: line = {lines[labNum]}")
-            line_split = lines[labNum].split(" ")
-            labelNum = int(line_split[0])
-            X = float(line_split[1]) * imgW
-            Y = float(line_split[2]) * imgH
-            w = float(line_split[3]) * imgW
-            h = float(line_split[4]) * imgH
 
-            upperLeft_X = int(X - w/2)
-            upperLeft_Y = int(Y - h/2)
-            lowerRight_X = int(X + w/2)
-            lowerRight_Y = int(Y + h/2)
+        #print(f"getLabBox: line = {lines[labNum]}")
+        line_split = lines[labNum].split(" ")
+        labelNum = int(line_split[0])
+        X = float(line_split[1]) * imgW
+        Y = float(line_split[2]) * imgH
+        w = float(line_split[3]) * imgW
+        h = float(line_split[4]) * imgH
 
-            UL = [upperLeft_X, upperLeft_Y]
-            LR = [lowerRight_X, lowerRight_Y]
+        # Moves to get lab box?
+        upperLeft_X = int(X - w/2)
+        upperLeft_Y = int(Y - h/2)
+        lowerRight_X = int(X + w/2)
+        lowerRight_Y = int(Y + h/2)
 
-            #print(f"label number: {line_split[0]}, {self.classes[labelNum]}")
-            return self.classes[labelNum], UL, LR
+        UL = [upperLeft_X, upperLeft_Y]
+        LR = [lowerRight_X, lowerRight_Y]
+
+        #print(f"label number: {line_split[0]}, {self.classes[labelNum]}")
+        return self.classes[labelNum], UL, LR
--- cv/training/viewer.py
+++ cv/training/viewer.py
@@ -6,39 +6,51 @@ import torchvision.transforms as trans
 
 from getLab import getLab
 
+def getThisLab(fileName):
+    print(f"getThisLab file: {fileName}")
 
-dataSet = 'coco8'
-testTrain = 'val' # train or val
+    image = cv2.imread(imageFile_str) 
+    nLab = labeler.getNLab(imageFile_str) # Sets up file structure for this file, and returns the number of lables
+    print(f"getThisLab num labels: {nLab}")
+    for thisLab in range(nLab):
+        label, UL, LR = labeler.getLabBox(thisLab)
+        print(f"label: {thisLab}, {label}")
+        cv2.rectangle(img=image, pt1=UL, pt2=LR, color=(0,255,0), thickness=1)
+        cv2.putText(img=image, text=label, org=UL, fontFace=cv2.FONT_HERSHEY_COMPLEX, fontScale=0.5, color=(255, 255, 255))
 
-# is this yolo or CNN?
+    numpy_arr = np.asarray(image)
+    cv2.imshow('', numpy_arr)
+    cv2.waitKey()
 
-# make a read label function
-# getNumLabels
-# getLabel(n)
+modelType = 'ssd' # is this yolo or ssd?
+#modelType = 'yolo' # is this yolo or ssd?
+#thisFile = None
+thisFile = 'dataset3_0.jpg'
 
-baseDir = "datasets" 
-image_dir = baseDir + '/' + dataSet + '/images/' + testTrain
-
-labeler = getLab('yolo', baseDir, dataSet) # ssd, or yolo
+if modelType == 'ssd':
+    dataSet = 'dataset_ver1'
+    testTrain = 'test' # train or test
+    baseDir = "../../data/version1" 
+    image_dir = baseDir + '/' + dataSet + '/' + testTrain
+else:
+    dataSet = 'coco8'
+    testTrain = 'val' # train or val
+    baseDir = "datasets" 
+    image_dir = baseDir + '/' + dataSet + '/images/' + testTrain
 
 print(f"{image_dir}")
-listing = os.scandir(image_dir)
-for file in listing:
-    if fnmatch.fnmatch(file, '*.jpg'):
-        imageFile_str =image_dir+'/'+file.name
-        print(f"{imageFile_str}")
-        #image = Image.open(imageFile_str, ) 
-        image = cv2.imread(imageFile_str) 
-        #image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+labeler = getLab(modelType, baseDir, dataSet) # ssd, or yolo
 
-        nLab = labeler.getNLab(imageFile_str) # Sets up file structure for this file, and returns the number of lables
-        for thisLab in range(nLab):
-            label, UL, LR = labeler.getLabBox(thisLab)
-            print(f"label: {thisLab}, {label}")
-            cv2.rectangle(img=image, pt1=UL, pt2=LR, color=(0,255,0), thickness=1)
-            cv2.putText(img=image, text=label, org=UL, fontFace=cv2.FONT_HERSHEY_COMPLEX, fontScale=0.5, color=(255, 255, 255))
-        #exit()
+if thisFile != None:
+    imageFile_str =image_dir+'/'+thisFile
+    getThisLab(imageFile_str)
 
-        numpy_arr = np.asarray(image)
-        cv2.imshow('', numpy_arr)
-        cv2.waitKey()
+else:
+    listing = os.scandir(image_dir)
+    for file in listing:
+        if fnmatch.fnmatch(file, '*.jpg'):
+
+            # TODO make function so we can add single image variant
+            imageFile_str =image_dir+'/'+file.name
+            #print(f"{imageFile_str}")
+            getThisLab(imageFile_str)
