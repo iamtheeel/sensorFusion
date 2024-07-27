fa79618f92848a22067662ae97b963287f91f9a6
Josh M <eel.josh@gmail.com>
2024-07-26 17:51:30 -0700

    Adjusted font

--- cv/running/display.py
+++ cv/running/display.py
@@ -29,21 +29,21 @@ class displayHandObject:
         objUL, objLR = dist.getBox(dist.grabObject)
         cv2.rectangle(img=thisImg, pt1=objUL, pt2=objLR, color=self.objectColor, thickness=1)
         cv2.circle(img=thisImg, center=dist.bestCenter, radius=5, color=self.objectColor, thickness=2) #BGR
-        cv2.putText(img=thisImg, text=objText, org=objUL, fontFace=cv2.FONT_HERSHEY_COMPLEX, fontScale=0.5, color=self.objectColor)
+        cv2.putText(img=thisImg, text=objText, org=objUL, fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=1, color=self.objectColor)
 
         # The Hand
         handText = f"Hand: {dist.handObject[4]:.2f}"
         handUL, handLR = dist.getBox(dist.handObject)
         cv2.rectangle(img=thisImg, pt1=handUL, pt2=handLR, color=self.handColor, thickness=1)
         cv2.circle(img=thisImg, center=dist.handCenter, radius=5, color=self.handColor, thickness=2) #BGR
-        cv2.putText(img=thisImg, text=handText, org=handUL, fontFace=cv2.FONT_HERSHEY_COMPLEX, fontScale=0.5, color=self.handColor)
+        cv2.putText(img=thisImg, text=handText, org=handUL, fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=1.0, color=self.handColor)
 
         # The distance
         distText =f"Distance: {dist.bestDist:.0f}mm"
         distUL =  (5,15)
         print(distText)
-        cv2.putText(img=thisImg, text=distText, org=distUL, fontFace=cv2.FONT_HERSHEY_COMPLEX, fontScale=0.5, color=self.lineColor)
         cv2.line(img=thisImg, pt1=dist.bestCenter, pt2=dist.handCenter, color=self.lineColor, thickness=1)
+        cv2.putText(img=thisImg, text=distText, org=distUL, fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=1, color=self.lineColor)
 
         cv2.imshow("", thisImg)
         cv2.waitKey()
