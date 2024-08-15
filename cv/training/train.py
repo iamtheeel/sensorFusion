e7787cb084529d8ddb1d13daf0e1a0c00ef7fd55
Joshua B. Mehlman <jmehlman@sfsu.edu>
2024-08-15 21:11:27 +0000

    Logger and config for trainer

--- cv/training/train.py
+++ cv/training/train.py
@@ -13,58 +13,67 @@
 
 from ultralytics import YOLO
 from torchinfo import summary
+import logging
+import sys
+import os
 
 from torch import cuda, backends
+
+## Configuration # From MICLab
+sys.path.insert(0, '..')
+from ConfigParser import ConfigParser
+config = ConfigParser(os.path.join(os.getcwd(), '../config.yaml'))
+configs = config.get_config()
 
 device = "cpu" 
 if cuda.is_available(): device = "cuda" 
 if backends.mps.is_available() and backends.mps.is_built(): device = "mps"
 
-print(f"setup: Device = {device}")
+## Logging
+debug = configs['debugs']['debug']
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+if debug == False:
+    logging.disable(level=logging.CRITICAL)
+    logger.disabled = True
 
-epochs = 300
-# Training data
-image_depth = 3
-#image_sz = 320 # Works
-#image_sz = 240: Faile 
-#image_sz = 160 #works
-#image_sz = 96 #works
-
-
-#dataSet = "datasets/combinedData.yaml" # 2 class
-dataSet = "../datasets/coco_withHand.yaml" # coco 81 class, hand and apple data only
-#dataSet = "/home/coder/datasets/cocoData_handAppleAdded.yaml" # coco 81 class, coco, hand, and apple data
-#Dataset 'datasets/coco_withHand.yaml' images not found ⚠️, missing path '/Users/theeel/Documents/school/MIC/sensorFusion/src/cv/datasets/coco_withHand/images/val'
-#dataSet = "datasets/foo.yaml"  # Single image set, not working
-#dataSet = "datasets/dataset_ver1.yaml" # small(er) training set
-#dataSet = "datasets/coco8.yaml"
-
-
-# Load a model
-# https://github.com/ultralytics/ultralytics/blob/main/ultralytics/cfg/models/
-#yoloModel = YOLO("models/yolov5n.yaml")
-#yoloModel = YOLO("weights/yolov5nu.pt")  # pre-trained with 300 epochs
-yoloModel = YOLO("../models/yolov5n.yaml").load("../weights/yolov5nu.pt")  # build from YAML and transfer weights
+logger.info(f"setup: Device = {device}")
 
 # Model settings
-transLearn = True
-image_sz = 640 # Was trained with
-freezeLayer = 10 # First 10 layers are the backbone (10: freezes 0-9)
+image_sz = max( configs['training']['imageSize'])
+dataSet = configs['training']['dataSet']
+# https://github.com/ultralytics/ultralytics/blob/main/ultralytics/cfg/models/
+modelFile = configs['training']['modelsDir'] +'/' + configs['training']['modelFile']
+weightsFile = configs['training']['weightsDir'] +'/' + configs['training']['weightsFile']
+
+#HyperPerams
+epochs = configs['training']['epochs']
+
+
+logger.info(f"Image Size: {image_sz}")
 
 # From: https://docs.ultralytics.com/yolov5/tutorials/transfer_learning_with_frozen_layers/#freeze-backbone
-if transLearn:
+if configs['training']['transLearn']:
+       yoloModel = YOLO(modelFile).load(weightsFile)  # build from YAML and transfer weights
+       freezeLayer = configs['training']['freezeLayer'] # First 10 layers are the backbone (10: freezes 0-9)
+
        freeze = [f"model.{x}." for x in range(freezeLayer)]  # which layers to freeze
-       print(f"Layers to freeze: {freeze}")
-       print("--------------------------------------")
+       logger.info(f"Layers to freeze: {freeze}")
+       logger.info("--------------------------------------")
        for k, v in yoloModel.named_parameters():
-              #print(f"k: {k}")
+              #logger.info(f"k: {k}")
               v.requires_grad = True
               if any(x in k for x in freeze):
-                     print(f"Freezing layer {k}")
+                     logger.info(f"Freezing layer {k}")
                      v.requires_grad = False
+elif configs['training']['transLearn']:
+       yoloModel = YOLO(modelFile)
+else:
+       yoloModel = YOLO(weightsFile)
 
 yoloModel.info(detailed=True)
 '''
+image_depth = 3
 modelSum = summary(model=yoloModel.model, 
                    #verbose=2,
                    input_size=(1, image_depth, image_sz, image_sz), # make sure this is "input_size", not "input_shape": must be square
@@ -78,3 +87,4 @@ modelSum = summary(model=yoloModel.model,
 results = yoloModel.train(hsv_h=1.0, plots=True, pretrained=transLearn, data=dataSet, epochs=epochs, imgsz=image_sz, device=device) # cpu, cuda, mps
 
 exit()
+print
